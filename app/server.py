0c45a6b61bb64082f0b8b47d4dcb51c852b4fc3f
almog1020 <tshukati4@gmail.com>
2025-12-08 21:16:07 +0200

    add auth router

---
+++ app/server.py
@@ -0,0 +1,28 @@
+
+
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
+
+from app.auth import auth
+from app.routers import users
+
+app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+app.include_router(users.router)
+app.include_router(auth.router)
+
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
+
+
+
+
