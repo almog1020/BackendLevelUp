d898e6884b4615430b609424b9bbb8a7026748b2
almog1020 <tshukati4@gmail.com>
2025-12-12 18:28:50 +0200

    move files and combine files

--- app/logic/etl.py
+++ app/logic/etl.py
@@ -10,7 +10,7 @@ import httpx
 from typing import Optional
 from datetime import datetime
 
-from schemas import Game, GamePrice
+from app.schemas import Game, GamePrice
 
 
 # ============== EXTRACT ==============
