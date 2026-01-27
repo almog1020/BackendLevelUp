70a765f86e387bdb357571ff9cdf9cc8fe43373b
almog1020 <tshukati4@gmail.com>
2026-01-27 20:06:04 +0200

    remove comment

--- app/db.py
+++ app/db.py
@@ -4,11 +4,10 @@ from sqlalchemy import Engine
 from sqlmodel import SQLModel
 
 # Use environment variable if available (for Docker), otherwise use localhost
-# postgresql_url = os.getenv(
-#     "DATABASE_URL",
-#     default="postgresql://Almog:1999@127.0.0.1:5432/levelup"
-# )
-postgresql_url="postgresql://Almog:1999@127.0.0.1:5432/levelup"
+postgresql_url = os.getenv(
+    "DATABASE_URL",
+    default="postgresql://Almog:1999@127.0.0.1:5432/levelup"
+)
 def create_db_and_tables(engine: Engine):
     SQLModel.metadata.create_all(engine)
 
